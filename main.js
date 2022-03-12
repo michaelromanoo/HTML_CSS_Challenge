49650889608553cffd4ddde84f11874489da28c7
michaelromanoo <20636897+michaelromanoo@users.noreply.github.com>
2022-03-12 14:31:40 +0800

    feat: use DOM to dynamically display menu items

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.getElementById("nav-menu");
+const menuItems = ["Products", "About", "Contact", "Free Trial"];
+
+// dynamically create nav menu items
+// for each menuItem, create a li item within the existing list
+menuItems.forEach((element) => {
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(element));
+  console.log("li", li);
+  navMenu.appendChild(li);
+});
+
+console.log("nav menu", navMenu);
 
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
